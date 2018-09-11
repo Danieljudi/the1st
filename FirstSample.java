0ecd07704d64a2163edd5361001eea0600925b8d
Danieljudi <41111907+Danieljudi@users.noreply.github.com>
2018-09-11 21:32:47 +0200

    up

--- FirstSample.java
+++ FirstSample.java
@@ -3,9 +3,7 @@ public class FirstSample
 {
    public static void main(String[] args)
    {
-      
-      System.out.println("siema");
+   System.out.println("siema");
    System.out.println("podaj nazwe ksiazki:");
-  
    }
 }
