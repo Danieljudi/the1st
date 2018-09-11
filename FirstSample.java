cc37d2a9bfbc662c31512da381bea97e5f525e94
Danieljudi <41111907+Danieljudi@users.noreply.github.com>
2018-09-11 21:18:44 +0200

    the1st

---
+++ FirstSample.java
@@ -0,0 +1,11 @@
+
+public class FirstSample 
+{
+   public static void main(String[] args)
+   {
+      
+      System.out.println("siema");
+   System.out.println("podaj nazwe ksiazki:");
+  
+   }
+}
